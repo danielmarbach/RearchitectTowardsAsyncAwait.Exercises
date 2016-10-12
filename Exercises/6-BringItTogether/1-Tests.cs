1e56dad568a169814d85e137bc2e8b628e62c5c1
danielmarbach <daniel.marbach@openplace.net>
2016-10-12 20:34:52 +0200

    Bring it together exercise

---
+++ Exercises/6-BringItTogether/1-Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Exercises.BringItTogether
+{
+    [TestFixture]
+    public class TestsPart1
+    {
+        static Random random = new Random();
+
+        [Test]
+        public void KickItOff()
+        {
+            // TODO: Make this code async, step by step
+            Highlevel();
+        }
+
+        public static void Highlevel()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    Midlevel();
+                }
+                catch (InvalidOperationException ex)
+                {
+                    ex.Message.Output();
+                }
+            }
+        }
+
+        public static void Midlevel()
+        {
+            LowLevel();
+        }
+
+        private static void LowLevel()
+        {
+            Thread.Sleep(20); // Very important work
+            if (random.Next(0, 10)%2 == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            Thread.Sleep(20); // Very important work
+        }
+    }
+}
